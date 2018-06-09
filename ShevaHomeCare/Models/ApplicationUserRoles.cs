dc43198c9ae5b8328724157e2cf30d44e90569df
Onotseike <onotseike@outlook.com>
2018-06-09 21:29:58 +0100

    Add project files.

---
+++ ShevaHomeCare/Models/ApplicationUserRoles.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace ShevaHomeCare.Models
+{
+    public enum ShevaUserType
+    {
+        Patient, CareGiver
+    }
+
+    public class ApplicationUserRoles : IdentityRole
+    {
+        public ShevaUserType UserType { get; set; }
+    }
+}
