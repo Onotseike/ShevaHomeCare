f7adb09d6ac1f9c1f78df63bafc68496d025a554
Onotseike <onotseike@outlook.com>
2018-06-12 13:27:50 +0100

    npm install commit for syncfusion

---
+++ ShevaHomeCare/gulpfile.js
@@ -0,0 +1,5 @@
+﻿var gulp = require('gulp');
+gulp.task('copy', function () {
+    gulp.src('./node_modules/syncfusion-javascript/**')
+        .pipe(gulp.dest('./wwwroot/lib/syncfusion-javascript'));
+});
